d9eadcd19d64ac3deacf6d4a64aedf45e0ff304d
nikolodio <136374878+nikolodio@users.noreply.github.com>
2023-07-01 21:49:20 +0300

    Add files via upload

---
+++ ZadDom002.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите координату x1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z1:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z2:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+
+Console.Write(d);
---
+++ ZadDom003.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("enter the number: ");
+int end = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= end; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
