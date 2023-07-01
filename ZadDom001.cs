0bb80c3a3db2c8910434244f3dea57bbfa24eb61
Nikolas White <nikolodio.k@gmail.com>
2023-07-01 18:17:36 +0300

    first version

---
+++ ZadDom001.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter the five-digit number: ");
+string? number = Console.ReadLine();
+void fiveDigit(string number)
+{
+
+    if (number![0] == number[4] || number[1] == number[3])
+    {
+        Console.Write($"Number {number} -> palindrom");
+    }
+    else
+    {
+        Console.Write($"Number {number} its not palindrom");
+    }
+}
+if (number!.Length == 5)
+{
+    fiveDigit(number);
+}
+else
+{
+    Console.WriteLine("enter another number");
+}
